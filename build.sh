e79691987502b23b6a6864f18a534b5f0e1a423b
Ratoriku <a1063021545@gmail.com>
2022-03-23 02:20:15 -0400

    branch

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ mkdir twrp
 cd twrp
 repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0
 repo sync
-git clone https://$GITID:$GITPWD@github.com/Ratoriku/android_device_xiaomi_lavender-twrp -b ofox device/xiaomi/lavender
+git clone https://$GITID:$GITPWD@github.com/Ratoriku/android_device_xiaomi_lavender-twrp -b twrp-9.0 device/xiaomi/lavender
 rm -rf bootable/recovery
 git clone https://github.com/Jabiyeff/android_bootable_recovery --depth=1 bootable/recovery
 
